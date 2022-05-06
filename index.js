27e30b753005c85827785707cded81346b113f46
suriyaaap13 <suriyaaap13@gmail.com>
2022-05-06 19:42:32 +0530

    Set up directory for the project

---
+++ index.js
@@ -0,0 +1,22 @@
+// requiring library
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+//////////////////////////// Middleware /////////////////////////
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+
+app.listen(3000, (err)=>{
+    if(err){console.log("Error",err);}
+    console.log("Server is up and running in PORT 3000");
+})
